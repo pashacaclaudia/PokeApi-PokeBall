93a90fd25b34ab301e8f7c6f885b1c6632db9e6d
pashacaclaudia <pashaca.claudia.carolina@iisgalvanimi.edu.it>
2020-05-27 16:07:00 +0000

    F

---
+++ serverNode/routes/users.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+const sql = require('mssql');
+var createError = require('http-errors');
+
+
+const config = {
+  user: 'pashaca.claudia',  //Vostro user name
+  password: 'xxx123#', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: 'pashaca.claudia', //(Nome del DB)
+}
+
+//Function to connect to database and execute query
+let executeQuery = function (res, query, next) {
+  sql.connect(config, function (err) {
+    if (err) { //Display error page
+      console.log("Error while connecting database :- " + err);
+      res.status(500).json({success: false, message:'Error while connecting database', error:err});
+      return;
+    }
+    var request = new sql.Request(); // create Request object
+    request.query(query, function (err, result) { //Display error page
+      if (err) {
+        console.log("Error while querying database :- " + err);
+        res.status(500).json({success: false, message:'Error while querying database', error:err});
+        sql.close();
+        return;
+      }
+      res.send(result); //Il vettore con i dati è nel campo recordset (puoi loggare result per verificare)
+      sql.close();
+    });
+
+  });
+}
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  
+});
+
+/*router.get('/search/:name', function (req, res, next) {
+  let sqlQuery = `select * from dbo.[cr-unit-attributes] where Unit = '${req.params.name}'`;
+  executeQuery(res, sqlQuery, next);
+  
+});*/
+router.get('/inserisci', function(req, res, next) {
+    res.render('inserisci');
+});
+
+
+router.post('/', function (req, res, next) {
+  let unit = req.body;
+  if (!unit) {
+    next(createError(400 , "Please provide a correct unit"));
+  }
+  sql.connect(config, err => {
+    let sqlRequest = new sql.Request();
+    let sqlInsert = `INSERT INTO PokeBall.Pokemon (Nome, indirizzo) VALUES ('${unit.Nome}','${unit.indirizzo}')`;
+    sqlRequest.query(sqlInsert, (error, results) => {
+        if (error) throw error;
+        sqlRequest.query(`SELECT * FROM PokeBall.Pokemon WHERE Unit = '${unit.Nome}'`, (err, result) => {
+            if (err) console.log(err);
+            res.send(result);
+        });
+    });
+  })
+});
+
+module.exports = router;
